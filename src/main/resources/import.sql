6a30268ba12d77920480f6f950ad67cd85ae5446
amit <ar.tripathy@gmail.com>
2015-10-10 19:01:57 -0400

    fixed tests

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into ROAMY.city(id,    name,       status,     created_by, created_on,         last_modified_by,   last_modified_on) values(1,     'Mumbai',   'Active',   'test',     current_timestamp,  'test',             current_timestamp);
+insert into ROAMY.city(id,    name,       status,     created_by, created_on,         last_modified_by,   last_modified_on) values(2,     'Pune',     'Active',   'test',     current_timestamp,  'test',             current_timestamp);
--- src/test/java/com/roamy/TestApplication.java
+++ src/test/java/com/roamy/TestApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Abhijit on 10/5/2015.
  */
+@Profile("unit-test")
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "com.roamy")
 @Configuration
