602dca2b95f83acb89d6b53a4a48318ebb59be41
abhijitab <abhijitab@gmail.com>
2016-05-02 00:05:34 -0400

    minor changes

--- src/test/java/com/roamy/dao/api/AlertNotificationRepositoryTest.java
+++ src/test/java/com/roamy/dao/api/AlertNotificationRepositoryTest.java
@@ -75,6 +75,7 @@ public class AlertNotificationRepositoryTest {
     @After
     public void tearDown() {
         alertNotificationRepository.deleteAll();
+        userRepository.deleteAll();
     }
 
     @Test
